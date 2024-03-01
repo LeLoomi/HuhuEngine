ead5f0e7add9c1d2549caf4a548f86975e2e4656
Eliah Lohr <lohr.eliah@gmail.com>
2024-03-01 17:40:48 +0100

    We can now "move" the camera (we warp everything around the camera point to make it look like that anyways)

--- first_app.cpp
+++ first_app.cpp
@@ -27,6 +27,8 @@ namespace huhu
     {
         SimpleRenderSystem simpleRenderSystem{huhuDevice, huhuRenderer.getSwapChainRenderPass()};
         HuhuCamera camera{};
+        // camera.setViewDirection(glm::vec3(0.f), glm::vec3(.5f, 0.f, 1.f));
+        camera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f));
 
         while (!huhuWindow.shouldClose())
         {
