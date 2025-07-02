8b30243d67ffc546e4d8656f1704dd5759c0de34
Eliah Lohr <lohr.eliah@gmail.com>
2025-07-02 22:12:46 +0200

    Adapted project to new environment.

--- build_shaders.sh
+++ build_shaders.sh
@@ -1,3 +1,4 @@
 cd shaders
+VULKAN_SDK_PATH="/Users/eliah/VulkanSDK/1.4.313.1/macOS"
 for file in *.frag; do echo "> ${file}"; ${VULKAN_SDK_PATH}/bin/glslc ${file} -o ${file}.spv; done
 for file in *.vert; do echo "> ${file}"; ${VULKAN_SDK_PATH}/bin/glslc ${file} -o ${file}.spv; done
